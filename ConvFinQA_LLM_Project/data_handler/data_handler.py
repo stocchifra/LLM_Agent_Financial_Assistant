bdddc417490c0f89331d3516aa983d1d924f46c0
fstocchi <francescostocchi12@gmail.com>
2025-04-09 17:17:11 +0100

    Apply pre-commit fixes (isort, etc.)

--- ConvFinQA_LLM_Project/data_handler/data_handler.py
+++ ConvFinQA_LLM_Project/data_handler/data_handler.py
@@ -1,17 +1,16 @@
-import pandas as pd
 import json
 
 data_path = "/Users/francescostocchi/ConvFinQA_LLM_Project/data/train.json"
 
 with open(data_path, "r") as f:
     data = json.load(f)
-    
-#print(data[0])
+
+# print(data[0])
 
 questions = [
     entry["qa"]["question"]
     for entry in data
     if "qa" in entry and "question" in entry["qa"]
 ]
-print(questions[0])       
-    
+
+print(questions[0])
